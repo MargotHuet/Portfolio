7cb3b310a4cf014aa29acea3c7792ee41b755f94
MargotHuet <margothuet4@gmail.com>
2023-08-06 21:34:02 +0200

    tailwind css grid update

--- portfolio/src/composants/presentation.jsx
+++ portfolio/src/composants/presentation.jsx
@@ -5,20 +5,21 @@ import Signature from '/Users/margothuet/Documents/Dev/Portfolio/portfolio/src/c
 const Presentation = () => {
     return (
         <div className="flex w-full px-8 m-8">
-            <div className="container px-8 py-5 bg-sky-100 text-black grid grid-cols-5 grid-rows-5 gap-1"> 
-            <div className="col-span-2 ">
-                 <h1 className="text-4xl">A propos de moi</h1>
+            <div className="container px-8 py-5 bg-sky-100 text-black grid grid-cols-5 grid-rows-3 gap-1"> 
+            <div className="col-span-3 ">
+                 <h1 className="">A propos de moi</h1>
                  </div>
-                 <div className="col-span-2 row-span-2 col-start-1 row-start-2">
-                 <p className="text-xl ">Je suis une étudiante passionnée d'intelligences artificielles à la recherche d'une alternance dans le domaine du développement. Mes compétences techniques en git, GitHub, IDE, HTML, CSS, JavaScript, Python, Jupyter Notebook, Pandas et NumPy me permettent de mener à bien des projets d'IA de A à Z. J'ai également acquis une expérience significative en participant à des concours sur Kaggle, ce qui m'a permis de perfectionner mes compétences en résolution de problèmes et en conception de modèles. Je suis avide d'apprendre et désireuse de contribuer activement au succès de votre entreprise. Merci pour l'opportunité que vous me donnerez de faire partie de votre équipe.</p>
+                 <div className="col-span-3 row-span-2 col-start-1 row-start-2">
+                 <p className="">Je suis une étudiante passionnée d'intelligences artificielles à la recherche d'une alternance dans le domaine du développement. Mes compétences techniques en git, GitHub, IDE, HTML, CSS, JavaScript, Python, Jupyter Notebook, Pandas et NumPy me permettent de mener à bien des projets d'IA de A à Z. J'ai également acquis une expérience significative en participant à des concours sur Kaggle, ce qui m'a permis de perfectionner mes compétences en résolution de problèmes et en conception de modèles. Je suis avide d'apprendre et désireuse de contribuer activement au succès de votre entreprise. Merci pour l'opportunité que vous me donnerez de faire partie de votre équipe.</p>
                  </div>
-                    <div className="row-span-2 col-start-3 row-start-1 ">
-                        <img className="" src={Portrait} alt="portrait" /> 
+                    <div className="col-span-2 row-span-2 col-start-4 row-start-1">
+                        <img className="w-72" src={Portrait} alt="portrait" /> 
                     </div>
-                    <div className="col-start-3 row-start-3">
-                        <img className="" src={Signature} alt="signature" />
+                    <div className="col-span-2 col-start-4 row-start-3">
+                        <img className="w-60" src={Signature} alt="signature" />
                     </div>
             </div>
+            
             <button className="flex justify-center">Mes projets</button>    
         </div>
     )
