fe3e07d0e0c573aa5048ab0223ecfa87ee20262f
MargotHuet <margothuet4@gmail.com>
2023-08-07 15:02:04 +0200

    update cloud animation background

--- portfolio/src/composants/accueil.jsx
+++ portfolio/src/composants/accueil.jsx
@@ -3,8 +3,7 @@ import MyImage from '/Users/margothuet/Documents/Dev/Portfolio/portfolio/src/com
 
 const MyComponent = () => {
     return (
-        <div className="w-full h-screen">
-            <img class="top-0 left-0 w-full h-screen object-cover" src={MyImage} alt="Banniere d'accueil" />  
+        <div className="">
         </div>
     );
   };
--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Clouds from './composants/cloud';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Clouds />
     <App />
   </React.StrictMode>
 );
