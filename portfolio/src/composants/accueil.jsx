7bf2fb29ab94c57d1cc3b172c1299eadd0735964
MargotHuet <margothuet4@gmail.com>
2023-08-07 16:55:37 +0200

    fixed cloud bugs

--- portfolio/src/composants/accueil.jsx
+++ portfolio/src/composants/accueil.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import MyImage from '/Users/margothuet/Documents/Dev/Portfolio/portfolio/src/composants/images/backgroundportfolio.jpg';
+import Cloud from "/Users/margothuet/Documents/Dev/Portfolio/portfolio/src/composants/images/cloud.png";
+
 
 const MyComponent = () => {
     return (
-        <div className="">
+        <div className="w-full h-screen ">
+            <img class="relative top-0 left-0 w-full h-screen bg-top" src={MyImage} alt="Banniere d'accueil" />  
+            <img className="absolute top-0 left-0 w-max h-4/5 animate-scrolling-image py-32" src={Cloud} alt="cloud banner"/>
         </div>
     );
   };
