c53341e4d3c6c22b82d7466e30ef73aee8308bc7
MargotHuet <margothuet4@gmail.com>
2023-08-08 12:32:10 +0200

    update button color

--- portfolio/src/composants/button.jsx
+++ portfolio/src/composants/button.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = () => {
     return (
-        <button className="transition ease-in-out delay-100 bg-white hover:-translate-y-1 hover:scale-110 hover:bg-sky-100 duration-200 shadow-lg shadow-sky-200 m-20">Mes projets</button>
+        <button className="transition ease-in-out  delay-100 bg-sky-400 hover:-translate-y-1 hover:scale-110 hover:bg-sky-300 duration-200 shadow-lg shadow-sky-200 m-20">Mes projets</button>
     )
 }
 
