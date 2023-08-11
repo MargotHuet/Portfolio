7a6d1d44ac8f0d3aeb1a599a9371df8a3f5029d2
MargotHuet <margothuet4@gmail.com>
2023-08-11 16:43:16 +0200

    update button

--- portfolio/src/composants/presentation.jsx
+++ portfolio/src/composants/presentation.jsx
@@ -13,13 +13,13 @@ const Presentation = () => {
                  <div className="col-span-3 row-span-2 col-start-1 row-start-2 flex justify-self-center px-20">
                  <p className="text-xl h-full w-full">Je suis une étudiante passionnée d'intelligences artificielles à la recherche d'une alternance dans le domaine du développement. Mes compétences techniques en git, GitHub, IDE, HTML, CSS, JavaScript, Python, Jupyter Notebook, Pandas et NumPy me permettent de mener à bien des projets d'IA de A à Z. J'ai également acquis une expérience significative en participant à des concours sur Kaggle, ce qui m'a permis de perfectionner mes compétences en résolution de problèmes et en conception de modèles. Je suis avide d'apprendre et désireuse de contribuer activement au succès de votre entreprise. Merci pour l'opportunité que vous me donnerez de faire partie de votre équipe.</p>
                  </div>
-                    <div className="col-span-2 row-span-2 col-start-4 row-start-1 py-32 flex justify-self-center">
+                    <div className="col-span-2 row-span-2 col-start-4 row-start-1 w-52 h-72 py-32 flex justify-self-center">
                         <img className="w-62 h-72" src={Portrait} alt="portrait" /> 
                     </div>
-                    <div className="col-span-2 col-start-4 row-start-3 flex justify-self-center">
-                        <img className="w-72 h-72 " src={Signature} alt="signature" />
+                    <div className="col-span-2 col-start-4 row-span-4 row-start-4 flex justify-self-center ">
+                        <img className="w-62 h-52" src={Signature} alt="signature" />
                     </div>
-                    <div className="w-screen px-20 py-10">
+                    <div className="w-screen px-48 py-10 flex justify-self-center ">
                         <button className="left-12">Contact</button>
                     </div>
             </div>
