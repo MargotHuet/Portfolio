a2c5b4b9d42746b5990e27c3ab1082c5df16eced
MargotHuet <margothuet4@gmail.com>
2023-08-26 19:38:10 +0200

    css modified

--- portfolio/src/composants/accueil.jsx
+++ portfolio/src/composants/accueil.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import Cloud from "/Users/margothuet/Documents/Dev/Portfolio/portfolio/src/composants/images/cloud.png";
-import Cloud2 from "/Users/margothuet/Documents/Dev/Portfolio/portfolio/src/composants/images/cloud2.png";
-import Cloud3 from "/Users/margothuet/Documents/Dev/Portfolio/portfolio/src/composants/images/cloud3.png";
 import Grey from "/Users/margothuet/Documents/Dev/Portfolio/portfolio/src/composants/images/grey.png";
+import Clouds from "/Users/margothuet/Documents/Dev/Portfolio/portfolio/src/composants/images/setofclouds.png";
 
 
 const MyComponent = () => {
     return (
         <div className="landing-page">
-            <img className="cloud" src={Cloud} alt="animated clouds"/>
-            <img className="cloud2" src={Cloud2} alt="animated cloud"/>
-            <img className="cloud3" src={Cloud3} alt="animated cloud2"/>
+            <img className="set-of-clouds" src={Clouds} alt="animated clouds"/>
             <img className="greyBox" src={Grey} alt="grey"></img>
             <p className="title">Margot Huet</p>
             <button>Présentation</button>
