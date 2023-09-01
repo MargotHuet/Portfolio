dca7a24c3161c7e4d193e30857a9966dda40d0fe
MargotHuet <margothuet4@gmail.com>
2023-09-01 14:28:33 +0200

    css update

--- portfolio/src/composants/accueil.jsx
+++ portfolio/src/composants/accueil.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import Grey from "/Users/margothuet/Documents/Dev/Portfolio/portfolio/src/composants/images/grey.png";
-import Clouds from "/Users/margothuet/Documents/Dev/Portfolio/portfolio/src/composants/images/setofclouds.png";
+import White from "/Users/margothuet/Documents/Dev/Portfolio/portfolio/src/composants/images/white.png";
+import Clouds from "/Users/margothuet/Documents/Dev/Portfolio/portfolio/src/composants/images/cloudsset.png";
 
 
 const MyComponent = () => {
     return (
         <div className="landing-page">
             <img className="set-of-clouds" src={Clouds} alt="animated clouds"/>
-            <img className="greyBox" src={Grey} alt="grey"></img>
+                <div className="white-container">
+                    <img className="whiteBox" src={White} alt="white box container"></img>
+                </div>
             <p className="title">Margot Huet</p>
             <button>Présentation</button>
             <button>Mes projets</button>
